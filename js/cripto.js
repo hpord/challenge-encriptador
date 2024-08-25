37c46efb5ac5a4b5d74c73a564f4d8c651f32f77
hpord <buho013@gmail.com>
2024-08-24 19:00:47 -0500

    se termina con las funciones de encriptacion, desencriptacion y reconocimiento de minusculas

--- js/cripto.js
+++ js/cripto.js
@@ -1,16 +1,32 @@
 export function encriptar(text) {
-    return [...text].map(c => {
-        switch (c) {
-            case 'a': return 'ai'; 
-            case 'e': return 'enter'; 
-            case 'i': return 'imes'; 
-            case 'o': return 'ober'; 
-            case 'u': return 'ufat'; 
-            default: return c;
-        }
-    }).join('');
+  return [...text]
+    .map((c) => {
+      switch (c) {
+        case "a":
+          return "ai";
+        case "e":
+          return "enter";
+        case "i":
+          return "imes";
+        case "o":
+          return "ober";
+        case "u":
+          return "ufat";
+        default:
+          return c;
+      }
+    })
+    .join("");
 }
 
 export function desencriptar(text) {
-    return "chau";
+  const reemplazos = {
+    enter: "e",
+    imes: "i",
+    ai: "a",
+    ober: "o",
+    ufat: "u",
+  };
+
+  return text.replace(/enter|imes|ai|ober|ufat/g, (match) => reemplazos[match]);
 }
--- js/main.js
+++ js/main.js
@@ -7,10 +7,11 @@ function asignarTextoElemento(elemento, texto) {
 }
 
 function verificarMinusculas(text) {
-  return /^[a-z]+$/.test(text);
+  return /^[a-z\s]+$/.test(text);
 }
 
 const botonEncriptar = document.querySelector(".encriptar");
+const botonDesencriptar = document.querySelector(".desencriptar");
 
 botonEncriptar.addEventListener("click", function () {
   let advertenciaParrafo = document.querySelector(".advertencia-parrafo");
@@ -22,8 +23,20 @@ botonEncriptar.addEventListener("click", function () {
     advertenciaParrafo.style.fontSize = "1rem";
     return;
   }
+  console.log(encriptar(text));
+  /*asignarTextoElemento('.result', encriptar(text));*/
+});
 
-  /*console.log(text);
-   console.log(encriptar(text));
-    asignarTextoElemento('.result', encriptar(text));*/
+botonDesencriptar.addEventListener("click", function () {
+  let advertenciaParrafo = document.querySelector(".advertencia-parrafo");
+  advertenciaParrafo.style.color = "#495057";
+  advertenciaParrafo.style.fontSize = "0.75rem";
+  const text = document.getElementById("entrada-texto").value;
+  if (!verificarMinusculas(text)) {
+    advertenciaParrafo.style.color = "#FF0000";
+    advertenciaParrafo.style.fontSize = "1rem";
+    return;
+  }
+  console.log(desencriptar(text));
+  /*asignarTextoElemento('.result', encriptar(text));*/
 });
